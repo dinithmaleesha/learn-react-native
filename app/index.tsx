3e478ceecd8b1b9def64f006b839b5702d350f16
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-11 15:48:07 +0530

    Part #1

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Index = () => {
+  return (
+   <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-5xl font-bold text-green-700'>Hello World</Text>
+    </View>
+  );
+}
+
+export default Index;
